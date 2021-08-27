58024f10e3671f2b5f7a85d41ec416e50be4aa62
Richie Zed <richie@richezed.com>
2021-08-27 23:05:59 +1000

    Fixed passing of parameters to squeezelite

--- builds/install_squeezelite.sh
+++ builds/install_squeezelite.sh
@@ -1,5 +1,5 @@
-LMS_SERVER=config_software_squeezelite_server
-PLAYER_NAME=config_software_squeezelite_playername
+LMS_SERVER=$config_software_squeezelite_server
+PLAYER_NAME=$config_software_squeezelite_playername
 
 wget -qO - squeezelite https://sourceforge.net/projects/lmsclients/files/squeezelite/linux/squeezelite-1.9.9.1386-armhf.tar.gz/download | tar -zxvf - squeezelite
 sudo mv squeezelite /usr/bin
@@ -18,7 +18,7 @@ echo | sudo tee -a /lib/systemd/system/squeezelite.service
 echo | sudo tee -a /lib/systemd/system/squeezelite.service
 echo User=pi | sudo tee -a /lib/systemd/system/squeezelite.service
 echo | sudo tee -a /lib/systemd/system/squeezelite.service
-echo ExecStart=/usr/bin/squeezelite -s "@LMS_SERVER" -n "@PLAYER_NAME" | sudo tee -a /lib/systemd/system/squeezelite.service
+echo ExecStart=/usr/bin/squeezelite -s "$LMS_SERVER" -n "$PLAYER_NAME" | sudo tee -a /lib/systemd/system/squeezelite.service
 echo | sudo tee -a /lib/systemd/system/squeezelite.service
 echo [Install] | sudo tee -a /lib/systemd/system/squeezelite.service
 echo WantedBy=multi-user.target | sudo tee -a /lib/systemd/system/squeezelite.service
